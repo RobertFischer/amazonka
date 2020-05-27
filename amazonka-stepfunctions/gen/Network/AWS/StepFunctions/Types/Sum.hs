ac35df39f1184315a22a5f8f5b504e221d26f06f
Robert Fischer <robert.fischer@k4connect.com>
2020-05-27 16:19:53 -0400

    Update the sum types to get all the EventHistoryType entries

--- amazonka-stepfunctions/gen/Network/AWS/StepFunctions/Types/Sum.hs
+++ amazonka-stepfunctions/gen/Network/AWS/StepFunctions/Types/Sum.hs
@@ -19,14 +19,13 @@ module Network.AWS.StepFunctions.Types.Sum where
 
 import Network.AWS.Prelude
 
-data ExecutionStatus
-  = Aborted
-  | Failed
-  | Running
-  | Succeeded
-  | TimedOut
-  deriving (Eq, Ord, Read, Show, Enum, Bounded, Data, Typeable, Generic)
-
+data ExecutionStatus = Aborted
+                     | Failed
+                     | Running
+                     | Succeeded
+                     | TimedOut
+                         deriving (Eq, Ord, Read, Show, Enum, Bounded, Data,
+                                   Typeable, Generic)
 
 instance FromText ExecutionStatus where
     parser = takeLowerText >>= \case
@@ -58,47 +57,63 @@ instance ToJSON ExecutionStatus where
 instance FromJSON ExecutionStatus where
     parseJSON = parseJSONText "ExecutionStatus"
 
-data HistoryEventType
-  = ActivityFailed
-  | ActivityScheduleFailed
-  | ActivityScheduled
-  | ActivityStarted
-  | ActivitySucceeded
-  | ActivityTimedOut
-  | ChoiceStateEntered
-  | ChoiceStateExited
-  | ExecutionAborted
-  | ExecutionFailed
-  | ExecutionStarted
-  | ExecutionSucceeded
-  | ExecutionTimedOut
-  | FailStateEntered
-  | LambdaFunctionFailed
-  | LambdaFunctionScheduleFailed
-  | LambdaFunctionScheduled
-  | LambdaFunctionStartFailed
-  | LambdaFunctionStarted
-  | LambdaFunctionSucceeded
-  | LambdaFunctionTimedOut
-  | ParallelStateAborted
-  | ParallelStateEntered
-  | ParallelStateExited
-  | ParallelStateFailed
-  | ParallelStateStarted
-  | ParallelStateSucceeded
-  | PassStateEntered
-  | PassStateExited
-  | SucceedStateEntered
-  | SucceedStateExited
-  | TaskStateAborted
-  | TaskStateEntered
-  | TaskStateExited
-  | TaskSubmitted
-  | WaitStateAborted
-  | WaitStateEntered
-  | WaitStateExited
-  deriving (Eq, Ord, Read, Show, Enum, Bounded, Data, Typeable, Generic)
-
+data HistoryEventType = ActivityFailed
+                      | ActivityScheduleFailed
+                      | ActivityScheduled
+                      | ActivityStarted
+                      | ActivitySucceeded
+                      | ActivityTimedOut
+                      | ChoiceStateEntered
+                      | ChoiceStateExited
+                      | ExecutionAborted
+                      | ExecutionFailed
+                      | ExecutionStarted
+                      | ExecutionSucceeded
+                      | ExecutionTimedOut
+                      | FailStateEntered
+                      | LambdaFunctionFailed
+                      | LambdaFunctionScheduleFailed
+                      | LambdaFunctionScheduled
+                      | LambdaFunctionStartFailed
+                      | LambdaFunctionStarted
+                      | LambdaFunctionSucceeded
+                      | LambdaFunctionTimedOut
+                      | MapIterationAborted
+                      | MapIterationFailed
+                      | MapIterationStarted
+                      | MapIterationSucceeded
+                      | MapStateAborted
+                      | MapStateEntered
+                      | MapStateExited
+                      | MapStateFailed
+                      | MapStateStarted
+                      | MapStateSucceeded
+                      | ParallelStateAborted
+                      | ParallelStateEntered
+                      | ParallelStateExited
+                      | ParallelStateFailed
+                      | ParallelStateStarted
+                      | ParallelStateSucceeded
+                      | PassStateEntered
+                      | PassStateExited
+                      | SucceedStateEntered
+                      | SucceedStateExited
+                      | TaskFailed
+                      | TaskScheduled
+                      | TaskStartFailed
+                      | TaskStarted
+                      | TaskStateAborted
+                      | TaskStateEntered
+                      | TaskStateExited
+                      | TaskSubmitFailed
+                      | TaskSubmitted
+                      | TaskSucceeded
+                      | TaskTimedOut
+                      | WaitStateAborted
+                      | WaitStateEntered
+                      | WaitStateExited
+                          deriving (Eq, Ord, Read, Show, Enum, Bounded, Data,
+                                    Typeable, Generic)
 
 instance FromText HistoryEventType where
     parser = takeLowerText >>= \case
@@ -123,6 +138,16 @@ instance FromText HistoryEventType where
         "lambdafunctionstarted" -> pure LambdaFunctionStarted
         "lambdafunctionsucceeded" -> pure LambdaFunctionSucceeded
         "lambdafunctiontimedout" -> pure LambdaFunctionTimedOut
+        "mapiterationaborted" -> pure MapIterationAborted
+        "mapiterationfailed" -> pure MapIterationFailed
+        "mapiterationstarted" -> pure MapIterationStarted
+        "mapiterationsucceeded" -> pure MapIterationSucceeded
+        "mapstateaborted" -> pure MapStateAborted
+        "mapstateentered" -> pure MapStateEntered
+        "mapstateexited" -> pure MapStateExited
+        "mapstatefailed" -> pure MapStateFailed
+        "mapstatestarted" -> pure MapStateStarted
+        "mapstatesucceeded" -> pure MapStateSucceeded
         "parallelstateaborted" -> pure ParallelStateAborted
         "parallelstateentered" -> pure ParallelStateEntered
         "parallelstateexited" -> pure ParallelStateExited
@@ -133,15 +158,22 @@ instance FromText HistoryEventType where
         "passstateexited" -> pure PassStateExited
         "succeedstateentered" -> pure SucceedStateEntered
         "succeedstateexited" -> pure SucceedStateExited
+        "taskfailed" -> pure TaskFailed
+        "taskscheduled" -> pure TaskScheduled
+        "taskstartfailed" -> pure TaskStartFailed
+        "taskstarted" -> pure TaskStarted
         "taskstateaborted" -> pure TaskStateAborted
         "taskstateentered" -> pure TaskStateEntered
         "taskstateexited" -> pure TaskStateExited
+        "tasksubmitfailed" -> pure TaskSubmitFailed
         "tasksubmitted" -> pure TaskSubmitted
+        "tasksucceeded" -> pure TaskSucceeded
+        "tasktimedout" -> pure TaskTimedOut
         "waitstateaborted" -> pure WaitStateAborted
         "waitstateentered" -> pure WaitStateEntered
         "waitstateexited" -> pure WaitStateExited
         e -> fromTextError $ "Failure parsing HistoryEventType from value: '" <> e
-           <> "'. Accepted values: activityfailed, activityschedulefailed, activityscheduled, activitystarted, activitysucceeded, activitytimedout, choicestateentered, choicestateexited, executionaborted, executionfailed, executionstarted, executionsucceeded, executiontimedout, failstateentered, lambdafunctionfailed, lambdafunctionschedulefailed, lambdafunctionscheduled, lambdafunctionstartfailed, lambdafunctionstarted, lambdafunctionsucceeded, lambdafunctiontimedout, parallelstateaborted, parallelstateentered, parallelstateexited, parallelstatefailed, parallelstatestarted, parallelstatesucceeded, passstateentered, passstateexited, succeedstateentered, succeedstateexited, taskstateaborted, taskstateentered, taskstateexited, tasksubmitted, waitstateaborted, waitstateentered, waitstateexited"
+           <> "'. Accepted values: activityfailed, activityschedulefailed, activityscheduled, activitystarted, activitysucceeded, activitytimedout, choicestateentered, choicestateexited, executionaborted, executionfailed, executionstarted, executionsucceeded, executiontimedout, failstateentered, lambdafunctionfailed, lambdafunctionschedulefailed, lambdafunctionscheduled, lambdafunctionstartfailed, lambdafunctionstarted, lambdafunctionsucceeded, lambdafunctiontimedout, mapiterationaborted, mapiterationfailed, mapiterationstarted, mapiterationsucceeded, mapstateaborted, mapstateentered, mapstateexited, mapstatefailed, mapstatestarted, mapstatesucceeded, parallelstateaborted, parallelstateentered, parallelstateexited, parallelstatefailed, parallelstatestarted, parallelstatesucceeded, passstateentered, passstateexited, succeedstateentered, succeedstateexited, taskfailed, taskscheduled, taskstartfailed, taskstarted, taskstateaborted, taskstateentered, taskstateexited, tasksubmitfailed, tasksubmitted, tasksucceeded, tasktimedout, waitstateaborted, waitstateentered, waitstateexited"
 
 instance ToText HistoryEventType where
     toText = \case
@@ -166,6 +198,16 @@ instance ToText HistoryEventType where
         LambdaFunctionStarted -> "LambdaFunctionStarted"
         LambdaFunctionSucceeded -> "LambdaFunctionSucceeded"
         LambdaFunctionTimedOut -> "LambdaFunctionTimedOut"
+        MapIterationAborted -> "MapIterationAborted"
+        MapIterationFailed -> "MapIterationFailed"
+        MapIterationStarted -> "MapIterationStarted"
+        MapIterationSucceeded -> "MapIterationSucceeded"
+        MapStateAborted -> "MapStateAborted"
+        MapStateEntered -> "MapStateEntered"
+        MapStateExited -> "MapStateExited"
+        MapStateFailed -> "MapStateFailed"
+        MapStateStarted -> "MapStateStarted"
+        MapStateSucceeded -> "MapStateSucceeded"
         ParallelStateAborted -> "ParallelStateAborted"
         ParallelStateEntered -> "ParallelStateEntered"
         ParallelStateExited -> "ParallelStateExited"
@@ -176,10 +218,17 @@ instance ToText HistoryEventType where
         PassStateExited -> "PassStateExited"
         SucceedStateEntered -> "SucceedStateEntered"
         SucceedStateExited -> "SucceedStateExited"
+        TaskFailed -> "TaskFailed"
+        TaskScheduled -> "TaskScheduled"
+        TaskStartFailed -> "TaskStartFailed"
+        TaskStarted -> "TaskStarted"
         TaskStateAborted -> "TaskStateAborted"
         TaskStateEntered -> "TaskStateEntered"
         TaskStateExited -> "TaskStateExited"
+        TaskSubmitFailed -> "TaskSubmitFailed"
         TaskSubmitted -> "TaskSubmitted"
+        TaskSucceeded -> "TaskSucceeded"
+        TaskTimedOut -> "TaskTimedOut"
         WaitStateAborted -> "WaitStateAborted"
         WaitStateEntered -> "WaitStateEntered"
         WaitStateExited -> "WaitStateExited"
@@ -193,11 +242,45 @@ instance ToHeader     HistoryEventType
 instance FromJSON HistoryEventType where
     parseJSON = parseJSONText "HistoryEventType"
 
-data StateMachineStatus
-  = Active
-  | Deleting
-  deriving (Eq, Ord, Read, Show, Enum, Bounded, Data, Typeable, Generic)
+data LogLevel = All
+              | Error'
+              | Fatal
+              | Off
+                  deriving (Eq, Ord, Read, Show, Enum, Bounded, Data,
+                            Typeable, Generic)
 
+instance FromText LogLevel where
+    parser = takeLowerText >>= \case
+        "all" -> pure All
+        "error" -> pure Error'
+        "fatal" -> pure Fatal
+        "off" -> pure Off
+        e -> fromTextError $ "Failure parsing LogLevel from value: '" <> e
+           <> "'. Accepted values: all, error, fatal, off"
+
+instance ToText LogLevel where
+    toText = \case
+        All -> "ALL"
+        Error' -> "ERROR"
+        Fatal -> "FATAL"
+        Off -> "OFF"
+
+instance Hashable     LogLevel
+instance NFData       LogLevel
+instance ToByteString LogLevel
+instance ToQuery      LogLevel
+instance ToHeader     LogLevel
+
+instance ToJSON LogLevel where
+    toJSON = toJSONText
+
+instance FromJSON LogLevel where
+    parseJSON = parseJSONText "LogLevel"
+
+data StateMachineStatus = Active
+                        | Deleting
+                            deriving (Eq, Ord, Read, Show, Enum, Bounded, Data,
+                                      Typeable, Generic)
 
 instance FromText StateMachineStatus where
     parser = takeLowerText >>= \case
@@ -219,3 +302,32 @@ instance ToHeader     StateMachineStatus
 
 instance FromJSON StateMachineStatus where
     parseJSON = parseJSONText "StateMachineStatus"
+
+data StateMachineType = Express
+                      | Standard
+                          deriving (Eq, Ord, Read, Show, Enum, Bounded, Data,
+                                    Typeable, Generic)
+
+instance FromText StateMachineType where
+    parser = takeLowerText >>= \case
+        "express" -> pure Express
+        "standard" -> pure Standard
+        e -> fromTextError $ "Failure parsing StateMachineType from value: '" <> e
+           <> "'. Accepted values: express, standard"
+
+instance ToText StateMachineType where
+    toText = \case
+        Express -> "EXPRESS"
+        Standard -> "STANDARD"
+
+instance Hashable     StateMachineType
+instance NFData       StateMachineType
+instance ToByteString StateMachineType
+instance ToQuery      StateMachineType
+instance ToHeader     StateMachineType
+
+instance ToJSON StateMachineType where
+    toJSON = toJSONText
+
+instance FromJSON StateMachineType where
+    parseJSON = parseJSONText "StateMachineType"
